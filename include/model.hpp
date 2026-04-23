0ea86ef5b45709c5f09dff1dc5680223beecb3e8
radek <boguslawskiradek@gmail.com>
2026-04-23 16:08:16 +0200

    Added Mesh and Model classes (used llm to generate code I didn't have any clue how get this thing working)

--- include/model.hpp
+++ include/model.hpp
@@ -1,18 +1,84 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#include <iostream>
-#include <string>
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+    glm::vec3 tangent;
+    glm::vec3 bitangent;
+};
 
-// TODO:
-// * Complete rework of model class with opengl tutorial ;))2
+struct Texture {
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class Mesh {
+private:
+    unsigned int VAO = 0;
+    unsigned int VBO = 0;
+    unsigned int EBO = 0;
+
+    void SetupMesh();
+
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, 
+         std::vector<Texture> textures);
+
+    void Draw(unsigned int shaderProgram) const;
+
+    ~Mesh();
+};
+
 class Model {
-    private:
-        const aiScene* model;
+public:
+    Model();
 
-    public:
-        Model(std::string model_path);
+    ~Model();
+
+    bool LoadModel(const std::string& path);
+
+    void Draw(unsigned int shaderProgram) const;
+
+    size_t GetMeshCount() const { return meshes.size(); }
+
+    const Mesh* GetMesh(size_t index) const;
+
+    const std::string& GetDirectory() const { return directory; }
+
+    const std::string& GetName() const { return name; }
+
+    void Clear();
+
+private:
+    std::vector<Mesh> meshes;
+    std::string directory;
+    std::string name;
+    std::vector<std::string> loadedTexturePaths;
+
+    void ProcessNode(aiNode* node, const aiScene* scene);
+
+    Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
+
+    std::vector<Texture> LoadMaterialTextures(aiMaterial* mat, aiTextureType type, 
+                                              const std::string& typeName);
+
+    unsigned int LoadTextureFromFile(const std::string& path);
+
+    bool IsTextureLoaded(const std::string& path) const;
 };
