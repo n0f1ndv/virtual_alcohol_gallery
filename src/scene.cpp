178b637b72cef300145baa89970cffb39631b4d7
radek <boguslawskiradek@gmail.com>
2026-05-07 10:17:21 +0200

    Added basic scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -16,40 +16,32 @@ Scene::~Scene() {
 }
 
 void Scene::DrawModels() {
-        models[0].Draw(program, 
-            glm::vec3(0.0f, -3.0f, 0.0f),   // position
-            glm::vec3(0.0f),                // rotation
-            glm::vec3(20.0f, 0.1f, 20.0f),  // scale
-            glm::vec3(0.2f,0.2f,0.2f)       // color
-        ); 
+    // Floor     
+    models[0].Draw(program, 
+        glm::vec3(0.0f, -3.0f, 0.0f),   // position
+        glm::vec3(0.0f),                // rotation
+        glm::vec3(30.0f, 0.1f, 30.0f),  // scale
+        glm::vec3(0.2f,0.2f,0.2f)       // color
+    ); 
 
-        models[0].Draw(program, 
-            glm::vec3(4.0f, -1.5f, 2.0f),
-            glm::vec3(0.0f),
-            glm::vec3(0.5f, 1.25f, 0.5f),
-            glm::vec3(0.8f,0.7f,0.4f)
-        ); 
-
-        models[1].Draw(program, 
-            glm::vec3(4.0f, -0.25f, 2.0f),
-            glm::vec3(-90.0f, 0.0f, 0.0f),
-            glm::vec3(0.06f, 0.06f, 0.06f),
-            glm::vec3(0.0f,0.5f,0.5f)
-        ); 
-
-        models[0].Draw(program, 
-            glm::vec3(4.0f, -1.5f, -2.0f),
-            glm::vec3(0.0f),
-            glm::vec3(0.5f, 1.25f, 0.5f),
-            glm::vec3(0.2f,0.2f,0.2f)
-        ); 
-
-        models[1].Draw(program, 
-            glm::vec3(4.0f, -0.25f, -2.0f),
-            glm::vec3(-90.0f, 0.0f, 0.0f),
-            glm::vec3(0.06f, 0.06f, 0.06f),
-            glm::vec3(1.0f,0.0f,0.0f)
-        );
+    // Bottles on stands
+    for (int j = 0; j <= 10; j += 10) {
+        for (int i = 0; i <= 50; i += 5) {
+            models[0].Draw(program, 
+                glm::vec3(-20.0f + i, -1.5f, -20.0f + j),
+                glm::vec3(0.0f),
+                glm::vec3(0.5f, 1.25f, 0.5f),
+                glm::vec3(0.8f,0.7f,0.4f)
+            ); 
+        
+            models[1].Draw(program, 
+                glm::vec3(-20.0f + i, -0.25f, -20.0f + j),
+                glm::vec3(-90.0f, 0.0f, 0.0f),
+                glm::vec3(0.06f, 0.06f, 0.06f),
+                glm::vec3(0.0f,0.5f,0.5f)
+            ); 
+        }
+    }
 }
 
 void Scene::DrawLights() {
