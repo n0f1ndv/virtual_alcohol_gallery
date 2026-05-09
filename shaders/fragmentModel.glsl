f2e7066a2d70cd25a69368a80630b682f75e8715
radek <boguslawskiradek@gmail.com>
2026-05-09 13:37:43 +0200

    Added simple post-processing effects

--- shaders/fragmentModel.glsl
+++ shaders/fragmentModel.glsl
@@ -9,12 +9,6 @@ uniform vec3 uViewPos;
 uniform sampler2D uTexture;
 uniform vec3 uColor;
 
-vec3 grayscale(vec3 color) {
-    float avg = (color.r + color.g + color.b) / 3;
-
-    return vec3(avg);
-}
-
 void main() {
     vec3 base = texture(uTexture, vTex).rgb;
 
--- shaders/fragmentPP.glsl
+++ shaders/fragmentPP.glsl
@@ -2,13 +2,38 @@
 
 in vec2 TexCoords;
 
-// TODO: I think I didn't set screenTexture but for some reason it works ;0;
 uniform sampler2D screenTexture;
 uniform float time;
 
+// TODO: I think I didn't set screenTexture but for some reason it works ;0;
+vec4 blink(float strength, vec3 color) {
+    float coeff = (sin(strength * time) + 1) / 2; 
+
+    return vec4(vec3(coeff * color), 1.0);
+}
+
+vec4 negative(vec3 color) {
+    return vec4((vec3(1.0) - color), 1.0);
+}
+
+vec4 grayscale(vec3 color) {
+    float avg = (color.r + color.g + color.b) / 3;
+
+    return vec4(avg, avg, avg, 1.0);
+}
+
+vec4 fluctuateColors(vec3 color) {
+    return vec4(sin(color.b * time), 0.0, cos(color.r * time), 1.0);
+}
+
 void main()
 { 
-    float strength = 5.0;
+    vec3 color = texture(screenTexture, TexCoords).rgb;
 
-    gl_FragColor = vec4(vec3((sin(strength * time) + 1) / 2 * (1.0 - texture(screenTexture, TexCoords))), 1.0);
+    // Uncomment only one at a time
+    // gl_FragColor = negative(color);
+    // gl_FragColor = grayscale(color);
+    // gl_FragColor = blink(2.5, color);
+    // gl_FragColor = fluctuateColors(color);
+    gl_FragColor = vec4(color, 1.0);
 }
